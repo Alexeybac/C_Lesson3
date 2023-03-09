86fe5c42e47b56a92155f7b71296aee754e98a52
Лёха <Alexbac@yandex.ru>
2023-03-09 21:41:38 +0300

    Сыкл

---
+++ C#Lection3/Cycles/Program.cs
@@ -0,0 +1,10 @@
+﻿string method4(string text, int count)
+{
+    string rez = string.Empty;
+    for(int i = 0; i<count; i++)
+    {
+        rez = rez + text;
+    }
+    return rez;
+}
+Console.Write(method4("z", 110));
