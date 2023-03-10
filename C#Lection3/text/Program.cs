574fca8a30a4a314c0d86c59c2a55b6cd0e83314
Лёха <Alexbac@yandex.ru>
2023-03-10 21:02:26 +0300

    Роза упала

---
+++ C#Lection3/text/Program.cs
@@ -0,0 +1,20 @@
+﻿string text = "А - роза упала, на  лапу азора";
+
+string repleace (string text, char oldtext, char newtext)
+{
+    string rezult = String.Empty;
+    int leng = text.Length;
+    for (int i = 0; i < leng; i++)
+    {
+        if(text[i] == oldtext) rezult = rezult + $"{newtext}";
+        else rezult = rezult + $"{text[i]}";
+    }
+    return rezult;
+}
+
+string ntext = repleace(text, ',', ' ');
+//Console.WriteLine (ntext);
+string stext = repleace(ntext, 'о', 'и');
+//Console.WriteLine (stext);
+string ttext = repleace(stext, 'р', 'Р');
+Console.WriteLine (ttext);
